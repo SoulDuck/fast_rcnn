b82bb8815081bcf01ed58660c387b21723877bdd
kimseongjung <plznw4me@naver.com>
2018-08-02 00:42:45 +0900

    fix method get_groundtruths

--- trainer.py
+++ trainer.py
@@ -309,6 +309,8 @@ class Trainer(object):
                     _ , boxes =sess.run([self.net.opt ,self.net.boxes] , feed_dict=feed_dict)
                     tot, reg, cls = sess.run([self.net.loss_, self.net.reg_loss, self.net.class_loss],
                                              feed_dict=feed_dict)
+                    exit()
+
 
 
                     losses_[0].append(reg)
